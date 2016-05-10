5d7f912854f48905439abff8cbb4631c3b9ddf3d
Andrew <Asaganda@gmail.com>
2016-05-10 18:06:35 -0400

    add app header

---
+++ app/imports/components/layouts/app_header_layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const AppHeaderLayout = (props) =>
+  <nav className="navbar navbar-default">
+    <div className="container">
+      <div className="navbar-header">
+        <h1 className="navbar-brand">{props.pageTitle}</h1>
+      </div>
+    </div>
+  </nav>
+
+
+  AppHeaderLayout.propTypes = {
+    pageTitle: React.PropTypes.string
+  }
+
+  AppHeaderLayout.defaultProps = {
+    pageTitle: "My Notes App"
+  }
--- app/imports/components/layouts/app_layout.jsx
+++ app/imports/components/layouts/app_layout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import { AppHeaderLayout } from './app_header_layout'
+
 export const AppLayout = () =>
   <div id="app-container">
+    <AppHeaderLayout pageTitle="My Custom Title"/>
     <div id="main-content" className="container">
       React placeholder
     </div>
   </div>
-  
