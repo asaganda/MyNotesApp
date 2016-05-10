2b9583641520e02987629b121220d5379657ecc9
Andrew <Asaganda@gmail.com>
2016-05-10 17:46:06 -0400

    add react

---
+++ app/imports/components/layouts/app_layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AppLayout = () =>
+  <div id="app-container">
+    <div id="main-content" className="container">
+      React placeholder
+    </div>
+  </div>
+  
